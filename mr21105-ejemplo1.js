59f87e10cf3e13838e17e998cd05a4529f5db4e8
DanielMijango <113495528+DanielMijango@users.noreply.github.com>
2024-09-14 15:12:29 -0600

    index commit

---
+++ mr21105-ejemplo1.js
@@ -0,0 +1,4 @@
+var ejemplo1=JSON.parse('{"fichero": {"ficha": {"nombre": "Antonio","apellido": "Barbero","direccion": "Calle de Los Andes, 45"}}}')
+
+ejemplo1JSON =JSON.stringify(ejemplo1);
+
---
+++ mr21105-ejemplo2.js
@@ -0,0 +1,3 @@
+var ejemplo2=JSON.parse('{"pais": {"departamento": {"cabecera": "San Salvador","habitantes": "1,700,000","altura": "545 MSNM"}}}')
+
+ejemplo2JSON =JSON.stringify(ejemplo2);
